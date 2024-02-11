b58d54313c90fbdf352342e8df92cb00ad8f629e
meenakshi <meenakshisharma7395@gmail.com>
2024-02-11 12:54:41 +0530

    test redux added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+var EXPENSES = [
+  {
+    "id":1,
+    "description":"burger",
+    "category":1,
+    "date":"20/11/2023",
+    "amount":"400"
+  }
+]
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Spend wise Dev
-        
-        </a>
-      </header>
+       <p>Spendwise</p>
     </div>
   );
 }
